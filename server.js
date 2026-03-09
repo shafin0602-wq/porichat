209835f46fda4d65551dd87a34805307ddbbb822
SHAFIN <shafin0602@gmail.com>
2026-03-09 21:40:09 +0600

    Refactor server.js for user management and banning

--- server.js
+++ server.js
@@ -9,58 +9,93 @@ const io = new Server(server, { maxHttpBufferSize: 1e7 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-let waitingUser = null;
-let activeRooms = {};
-let onlineCount = 0;
+let queue = [];
+let users = {};
+let bannedIPs = new Set();
 
 io.on('connection', (socket) => {
-    onlineCount++;
-    io.emit('updateCount', onlineCount);
+    const userIP = socket.handshake.address;
+
+    // ব্যান চেক
+    if (bannedIPs.has(userIP)) {
+        socket.emit('error_msg', 'You are permanently banned.');
+        socket.disconnect();
+        return;
+    }
+
+    users[socket.id] = { id: socket.id, ip: userIP, room: null, partner: null, status: 'Idle' };
+    updateAdminStats();
 
     socket.on('findPartner', () => {
-        if (waitingUser && waitingUser.id !== socket.id) {
-            const partner = waitingUser;
-            waitingUser = null;
-            const roomName = `room-${socket.id}-${partner.id}`;
+        users[socket.id].status = 'Searching';
+        queue = queue.filter(id => id !== socket.id);
+
+        if (queue.length > 0) {
+            const partnerId = queue.shift();
+            const roomName = `room_${socket.id}_${partnerId}`;
             socket.join(roomName);
-            partner.join(roomName);
-            activeRooms[socket.id] = { room: roomName, partner: partner.id };
-            activeRooms[partner.id] = { room: roomName, partner: socket.id };
-            io.to(roomName).emit('matched', roomName);
+            const partnerSocket = io.sockets.sockets.get(partnerId);
+            
+            if (partnerSocket) {
+                partnerSocket.join(roomName);
+                users[socket.id].room = roomName;
+                users[socket.id].partner = partnerId;
+                users[socket.id].status = 'Chatting';
+                users[partnerId].room = roomName;
+                users[partnerId].partner = socket.id;
+                users[partnerId].status = 'Chatting';
+                io.to(roomName).emit('matched');
+            } else {
+                queue.push(socket.id);
+            }
         } else {
-            waitingUser = socket;
-            socket.emit('waiting', 'Searching for a Pori...');
+            queue.push(socket.id);
         }
+        updateAdminStats();
     });
 
-    socket.on('newMessage', (data) => {
-        socket.to(data.room).emit('receiveMessage', { type: 'text', content: data.message });
+    socket.on('message', (msg) => {
+        const user = users[socket.id];
+        if (user && user.room) socket.to(user.room).emit('message', { type: 'text', text: msg });
     });
 
-    socket.on('sendImage', (data) => {
-        socket.to(data.room).emit('receiveMessage', { type: 'image', content: data.image });
+    // অ্যাডমিন কমান্ডস
+    socket.on('admin_kick', (targetId) => {
+        const target = io.sockets.sockets.get(targetId);
+        if (target) target.disconnect();
     });
 
-    socket.on('typing', (data) => {
-        socket.to(data.room).emit('partnerTyping', true);
-    });
-
-    socket.on('stopTyping', (data) => {
-        socket.to(data.room).emit('partnerTyping', false);
+    socket.on('admin_ban', (targetId) => {
+        if (users[targetId]) {
+            bannedIPs.add(users[targetId].ip);
+            const target = io.sockets.sockets.get(targetId);
+            if (target) target.disconnect();
+        }
     });
 
     socket.on('disconnect', () => {
-        onlineCount--;
-        io.emit('updateCount', onlineCount);
-        const roomData = activeRooms[socket.id];
-        if (roomData) {
-            io.to(roomData.room).emit('partnerDisconnected');
-            delete activeRooms[roomData.partner];
-            delete activeRooms[socket.id];
+        const user = users[socket.id];
+        if (user && user.partner) {
+            const partnerSocket = io.sockets.sockets.get(user.partner);
+            if (partnerSocket) {
+                users[user.partner].room = null;
+                users[user.partner].partner = null;
+                users[user.partner].status = 'Idle';
+                partnerSocket.emit('partnerLeft');
+            }
         }
-        if (waitingUser && waitingUser.id === socket.id) waitingUser = null;
+        queue = queue.filter(id => id !== socket.id);
+        delete users[socket.id];
+        updateAdminStats();
     });
 });
 
+function updateAdminStats() {
+    io.emit('admin_data', {
+        onlineCount: Object.keys(users).length,
+        users: Object.values(users)
+    });
+}
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`PoriChat Pro+ live`));
+server.listen(PORT, () => console.log('PoriChat Server Active'));
